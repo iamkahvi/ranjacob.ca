184e3fe4e8108a5dc38cd2fdf854495882ce5f9c
Kahvi Patel <kahvi.patel@shopify.com>
2022-07-24 23:18:32 -0700

    Adding scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=production jekyll/jekyll:3.8 jekyll build
---
+++ sync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -azP _site root@142.93.152.73:/var/www/ranjacob.ca
